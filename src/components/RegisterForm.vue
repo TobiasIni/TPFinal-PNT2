2deb91983f3abac6bf65d58272bd915c7068073d
AndresSeminara <andresseminara@hotmail.com>
2024-06-26 18:23:00 -0300

    Agrego API de localizacion ara autocompletar direccion

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="register-form-container">
     <h1>Registro</h1>
-    <form  @submit.prevent="register" class="register-form">
+    <form @submit.prevent="register" class="register-form">
       <input v-model="username" type="text" placeholder="Username" class="register-input" required/>
       <input v-model="email" type="email" placeholder="Email" class="register-input" required/>
-      <input v-model="password" type="password" placeholder="Password" class="register-input" required/>
-      <input v-model="location" type="text" placeholder="location" class="register-input" required/>
+      <input ref="locationInput" v-model="location" type="text" placeholder="Location" class="register-input" required/>
       <select v-model="role" class="register-input" required>
         <option disabled value="">Seleccione un rol</option>
         <option v-for="role in roles" :key="role" :value="role">{{ role }}</option>
@@ -14,35 +13,47 @@
     </form>
   </div>
 </template>
-<script>
 
+<script>
 import { useAuthStore } from '../stores/authStore'
-import {Roles} from '../constants/roles.js'
+import { Roles } from '../constants/roles.js'
 
 export default {
-  data(){
+  data() {
     return {
-        username: '',
-        email: '',
-        password: '',
-        location:'',
-        role: Roles.USER,
-        roles: Object.values(Roles)
-      };
+      username: '',
+      email: '',
+      password: '',
+      location: '',
+      role: Roles.USER,
+      roles: Object.values(Roles)
+    };
+  },
+  mounted() {
+    this.initAutocomplete();
+  },
+  methods: {
+    async register() {
+      const authStore = useAuthStore();
+      await authStore.register(this.username, this.email, this.password, this.location, this.role);
+      if (authStore.isAuthenticated) {
+        this.$router.push({ name: 'Home' });
+      }
     },
-    methods:{
-        async register() {
-            const authStore = useAuthStore();
-            await authStore.register(this.username, this.email, this.password, this.location, this.role);
-            console.log(this.role)
-            if (authStore.isAuthenticated){
-                this.$router.push({ name: 'Home'})
-            }
-        }
+    initAutocomplete() {
+      const locationInput = this.$refs.locationInput;
+      const autocomplete = new google.maps.places.Autocomplete(locationInput, {
+        types: ['geocode']
+      });
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        this.location = place.formatted_address;
+      });
     }
-    
+  }
 }
 </script>
+
 <style scoped>
 .register-form-container {
   max-width: 500px;
