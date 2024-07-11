b828ef0b7cd610b461d3e122609d8280474bd073
Juan Francisco Pagés <jfranciscopages@gmail.com>
2024-07-10 22:20:57 -0300

    jfran cambios

--- src/components/Evento.vue
+++ src/components/Evento.vue
@@ -1,48 +1,50 @@
 <template>
   <div class="event-card" v-if="evento">
-      <img :src="evento.imageUrl || 'default-image-url.jpg'" :alt="evento.name || 'No Name Available'" class="event-image" />
-      <div class="event-info">
-          <h2>{{ evento.name || 'No Name Available' }}</h2>
-          <p>${{ evento.price || 'N/A' }}</p>
-      </div>
-      <button @click="cambiarCarrito" class="cart-button">
-          {{ enCarrito ? 'Sacar del carrito' : 'Agregar al carrito' }}
-      </button>
-      <button @click="$router.push({name: 'Detalles', state:{evento: evento}})" class="detail-button">
-        Ver Detalles
-      </button>
+    <img :src="evento.imageUrl || 'default-image-url.jpg'" :alt="evento.name || 'No Name Available'"
+      class="event-image" />
+    <div class="event-info">
+      <h2>{{ evento.name || 'No Name Available' }}</h2>
+      <p>${{ evento.price || 'N/A' }}</p>
+    </div>
+    <button @click="cambiarCarrito" class="cart-button">
+      {{ enCarrito ? 'Sacar del carrito' : 'Agregar al carrito' }}
+    </button>
+    <button @click="setDetalles" class="detail-button">
+      Ver Detalles
+    </button>
   </div>
   <div v-else>
-      <p>Loading...</p>
+    <p>Loading...</p>
   </div>
 </template>
 <script>
 import { useEventStore } from '../stores/eventStore.js';
 
 export default {
-    props:{
-        evento: Object
-    },
-    computed: {
-        enCarrito() {
-            const eventStore = useEventStore();
-            return eventStore.estaEnCarrito(this.evento.id)
-        }
-    },
-    methods: {
-        cambiarCarrito(){
-            const eventStore = useEventStore();
-            if(this.enCarrito) {
-                eventStore.removerDelCarrito(this.evento);
-            }else{
-                eventStore.agregarAlCarrito(this.evento);
-            }
-        },
-        obtenerDetalles(){
-          const eventStore = useEventStore()
-          eventStore.obtenerDetalles(this.evento)
-        }
+  props: {
+    evento: Object
+  },
+  computed: {
+    enCarrito() {
+      const eventStore = useEventStore();
+      return eventStore.estaEnCarrito(this.evento.id)
     }
+  },
+  methods: {
+    cambiarCarrito() {
+      const eventStore = useEventStore();
+      if (this.enCarrito) {
+        eventStore.removerDelCarrito(this.evento);
+      } else {
+        eventStore.agregarAlCarrito(this.evento);
+      }
+    },
+    setDetalles() {
+      const eventStore = useEventStore();
+      eventStore.vistaDetalles(this.evento); 
+      this.$router.push({ name: 'Detalles' })
+    }
+  }
 }
 </script>
 <style scoped>
@@ -87,9 +89,8 @@ button {
   margin-top: 16px;
 }
 
-.event-card:hover .cart-button, .event-card:hover .detail-button {
+.event-card:hover .cart-button,
+.event-card:hover .detail-button {
   background-color: #0056b3;
 }
 </style>
-
-  
